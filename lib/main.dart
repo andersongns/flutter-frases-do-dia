f0d64d7bfd3ad5588917436f6ea53a542d4761c4
Anderson Santos <anderson.a.santos@accenture.com>
2020-09-12 23:49:35 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+var home = Container(color: Colors.white);
+
+var app = MaterialApp(
+  title: 'Frases do dia',
+  home: home,
+);
+
+void main() {
+  runApp(app);
+}
